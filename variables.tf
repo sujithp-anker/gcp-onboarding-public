c200ff887198efd694b5edf2289caa61d5992ac9
mukesh <mukesh.muthukumar@ankercloud.com>
2026-04-13 12:55:01 +0530

    version12

--- variables.tf
+++ variables.tf
@@ -1,24 +1,26 @@
 variable "display_name" {
   type        = string
   description = "Display name for alerts"
+  default = ""
 }
 
 variable "email_address" {
   type        = string
   description = "Email for alerts"
+  default = ""
 }
 
-variable "days_in_cycle" {
-  type        = number
-  description = "Snapshot frequency"
-}
+# variable "days_in_cycle" {
+#   type        = number
+#   description = "Snapshot frequency"
+# }
 
-variable "start_time" {
-  type        = number
-  description = "Snapshot start time (hour)"
-}
+# variable "start_time" {
+#   type        = number
+#   description = "Snapshot start time (hour)"
+# }
 
-variable "retention_days" {
-  type        = number
-  description = "Retention period"
-}
+# variable "retention_days" {
+#   type        = number
+#   description = "Retention period"
+# }
