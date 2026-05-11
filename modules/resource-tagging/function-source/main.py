29731f074e073573102a71d4d0bc28c4579bc85e
mukesh <mukesh.muthukumar@ankercloud.com>
2026-05-11 20:40:02 +0530

    labels

--- modules/resource-tagging/function-source/main.py
+++ modules/resource-tagging/function-source/main.py
@@ -57,20 +57,20 @@ def main(event, context):
 
             handle_cloud_run(asset, LABELS)
 
+        elif asset_type == "run.googleapis.com/Job":
+
+            handle_cloud_run_job(asset, LABELS)
+
         elif "cloudfunction" in asset_type.lower():
 
             print("Cloud Function detected")
 
             handle_cloud_function(asset, LABELS)
 
-        elif asset_type == "run.googleapis.com/Job":
-
-            handle_cloud_run_job(asset, LABELS)    
-
         else:
 
             print("Unsupported asset type")
- "run.googleapis.com/Job"
+
     except Exception as e:
 
         print(f"ERROR: {str(e)}")
