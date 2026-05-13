1480a7d24cbadef30d9862cbec87271e4c518cf5
sujithp-anker <sujith.parthiban@ankercloud.com>
2026-05-13 17:46:09 +0530

    some modifications

--- providers.tf
+++ providers.tf
@@ -2,7 +2,16 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 7.0"
+      version = "~> 5.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
     }
   }
+}
+
+provider "google" {
+  project = var.Project_Id
+  region  = var.Region
 }
