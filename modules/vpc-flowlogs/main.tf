133602a5ecc438fa492f12282350e4063a3876bb
sujithp-anker <sujith.parthiban@ankercloud.com>
2026-05-13 16:15:19 +0530

    some modifications

--- modules/vpc-flowlogs/main.tf
+++ modules/vpc-flowlogs/main.tf
@@ -1,31 +1,29 @@
+locals {
+  network_pairs = [
+    for item in var.vpc_names : {
+      vpc    = split("/", item)[0]
+      subnet = split("/", item)[1]
+    }
+  ]
+}
+
 data "google_compute_subnetwork" "all" {
-  for_each = toset(var.vpc_names)
-  name     = each.value
+  for_each = { for pair in local.network_pairs : pair.subnet => pair }
+  name     = each.value.subnet
   region   = var.region
 }
 
 resource "google_compute_subnetwork" "network_logging" {
-  for_each = toset(var.vpc_names)
+  for_each = { for pair in local.network_pairs : pair.subnet => pair }
   
-  name          = each.value
-  region        = var.region
-  network       = each.value
-  
-  project       = var.project_id
+  name    = each.value.subnet
+  region  = var.region
+  project = var.project_id
+  network = each.value.vpc # Now correctly identifies the parent VPC
 
   log_config {
     aggregation_interval = "INTERVAL_5_SEC"
     flow_sampling        = 0.5
     metadata             = "INCLUDE_ALL_METADATA"
-    
   }
-}
-
-resource "google_logging_project_bucket_config" "vpc_flow_log_bucket" {
-  count      = var.enable_vpc_flow_logs ? 1 : 0
-  project    = var.project_id
-  location   = "global"
-  bucket_id  = "${var.customer_name}-vpc-flow-logs"
-  
-  retention_days = var.environment == "Prod" ? 30 : 7
 }
